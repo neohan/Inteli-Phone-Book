c405a8aff5c538544fdadcbc0cf8c7159a21bda2
neohan <neo_han@sohu.com>
2015-05-27 23:34:19 +0800

    创建ClickToDial拨号类。

---
+++ InteliPhoneBook/InteliPhoneBook.Model/ClickToDial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InteliPhoneBook.Model
+{
+    class ClickToDial
+    {
+        public string SIPGatewayIP;
+        public string SIPGatewayPort;
+        public string SIPServerIP;
+        public string SIPServerPort;
+        public string Ani;
+        public string Dnis;
+    }
+}
