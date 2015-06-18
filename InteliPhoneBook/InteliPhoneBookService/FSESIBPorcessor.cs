76bde67b48af6baa95f83af289623b81c4867ce9
neohan <neo_han@sohu.com>
2015-06-18 13:31:48 +0800

    增加重发create_uuid请求处理。配制文件中也相应增加了一个配制参数：重发create_uuid请求的上限。

--- InteliPhoneBook/InteliPhoneBookService/FSESIBPorcessor.cs
+++ InteliPhoneBook/InteliPhoneBookService/FSESIBPorcessor.cs
@@ -1,5 +1,7 @@
 ﻿//
 //负责流程：外呼桥接。
+
+//用户是否具备外线拨打权限不在此处处理，所以提交给此程序的外呼请求均认定为具备拨打权限。
 //
 using System;
 using System.Collections.Generic;
@@ -19,6 +21,7 @@ namespace InteliPhoneBookService
         #region configuration data
         public int FSESLInboundModeServerPort = 0;                   //fs inbound mode server port
         public int FSESLInboundModeReconnectTimes = 0;                  //fs inbound mode reconnect times
+        public int FSESLInboundModeRecreateUUIDTimes = 0;
         #endregion
 
         public void Initialize()
@@ -36,7 +39,9 @@ namespace InteliPhoneBookService
                 try { FSESLInboundModeServerPort = Int32.Parse(ConfigurationManager.AppSettings["FSESLInboundModeServerPort"]); }
                 catch (Exception e) { FSESLInboundModeServerPort = 8021; } log.Info("FreeSWITCH ESL InboundMode Server Port:" + FSESLInboundModeServerPort);
                 try { FSESLInboundModeReconnectTimes = Int32.Parse(ConfigurationManager.AppSettings["FSESLInboundModeReconnectTimes"]); }
-                catch (Exception e) { FSESLInboundModeReconnectTimes = 3; } log.Info("FreeSWITCH ESL InboundMode Reconnect Times:" + FSESLInboundModeReconnectTimes);
+                catch (Exception e) { FSESLInboundModeReconnectTimes = 3; } log.Info("FreeSWITCH ESL InboundMode Reconnect Limit:" + FSESLInboundModeReconnectTimes);
+                try { FSESLInboundModeRecreateUUIDTimes = Int32.Parse(ConfigurationManager.AppSettings["FSESLInboundModeRecreateUUIDTimes"]); }
+                catch (Exception e) { FSESLInboundModeRecreateUUIDTimes = 3; } log.Info("FreeSWITCH ESL InboundMode Re-create UUID Limit:" + FSESLInboundModeRecreateUUIDTimes);
             }
         }
 
@@ -62,27 +67,44 @@ namespace InteliPhoneBookService
             string Ani = clickToDial.Ani, Dnis = clickToDial.Dnis;
             string StateStr = "NONE", PrefixStr = "sofia/external/";
             while (true)
-            {//如果连不上fs也要求记录一下状态,利于诊断问题,这些信息同样也需要在街面上显示。
+            {//如果连不上fs也要求记录一下状态,利于诊断问题,这些信息同样也需要在界面上显示。
                 ESLconnection eslConnection = new ESLconnection(clickToDial.SIPGatewayIP, FSESIBProcessor.FSESIBProcessorObj.FSESLInboundModeServerPort, "ClueCon");
                 if (eslConnection.Connected() != ESL_SUCCESS)
                 {
+                    clickToDial.CurrentStatus = "CONNECTING";
                     log.Info(String.Format("task:{0}.Error connecting to FreeSwitch, do it again later.\r\n", clickToDial.TaskID));
                     ++reconnectTimes;
                     if (reconnectTimes > FSESIBProcessor.FSESIBProcessorObj.FSESLInboundModeReconnectTimes)
-                    { log.Info(String.Format("task:{0} exceed limit\r\n", clickToDial.TaskID)); break; }
+                    { log.Info(String.Format("task:{0} exceed limit\r\n", clickToDial.TaskID)); clickToDial.CurrentStatus = "EXCEEDLIMIT"; break; }
                     Thread.Sleep(1000); continue;
                 }
                 ESLevent eslEvent = eslConnection.SendRecv("event plain ALL");
                 if (eslEvent == null)
                 {
+                    clickToDial.CurrentStatus = "CONNECTED";
                     log.Info(String.Format("task:{0}.Error subscribing to all events.\r\n", clickToDial.TaskID));
                     ++reconnectTimes;
                     if (reconnectTimes > FSESIBProcessor.FSESIBProcessorObj.FSESLInboundModeReconnectTimes)
-                    { log.Info(String.Format("task:{0} exceed limit\r\n", clickToDial.TaskID));break; }
+                    { log.Info(String.Format("task:{0} exceed limit\r\n", clickToDial.TaskID)); clickToDial.CurrentStatus = "EXCEEDLIMIT"; break; }
                     Thread.Sleep(1000); continue;
                 }
                 //发起呼叫前，检索拨号计划，进行相应处理。
-                eslEvent = eslConnection.Api("create_uuid", String.Empty);
+                int recreateTimes = 0;
+                do
+                {
+                    ++recreateTimes;
+                    eslEvent = eslConnection.Api("create_uuid", String.Empty);
+                    if (eslEvent == null)
+                    {
+                        clickToDial.CurrentStatus = "PROCESSING";
+                        log.Info(String.Format("task:{0}.Error create_uuid.\r\n", clickToDial.TaskID));
+                        if (recreateTimes > FSESIBProcessor.FSESIBProcessorObj.FSESLInboundModeRecreateUUIDTimes)
+                        { log.Info(String.Format("task:{0} re-create UUID exceed limit\r\n", clickToDial.TaskID)); break; }
+                        Thread.Sleep(500);
+                    }
+                }
+                while (eslEvent == null);
+                if (eslEvent == null) continue;
                 string originateUuid = eslEvent.GetBody();
                 log.Info("create_uuid:" + originateUuid + "\r\n");
                 clickToDial.Uuid = originateUuid;
