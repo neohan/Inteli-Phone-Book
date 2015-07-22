ffbdba94b2206fced7a34e53ed13311409eab7e6
neohan <neo_han@sohu.com>
2015-07-22 22:16:13 +0800

    增加读许可文件。解码字串。

--- InteliPhoneBook/InteliPhoneBookService/LicProcessor.cs
+++ InteliPhoneBook/InteliPhoneBookService/LicProcessor.cs
@@ -5,11 +5,19 @@ using System.Text;
 using System.Threading;
 using System.Data;
 using System.Data.SqlClient;
+using System.Xml;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace InteliPhoneBookService
 {
     class LicProcessor
     {
+        private static byte[] ProjectInsideKeys = { 0xAA, 0xDF, 0x29, 0x5C, 0x36, 0x90, 0x17, 0x68 };
+        private static string ProjectInsideAddKey = "projectINSIDEkey";
+        private static byte[] Keys = { 0xA5, 0x95, 0x6E, 0x44, 0x80, 0xCB, 0x8F, 0x77 };
+        private static string AddKey = "";
+
         private const string INSERT_TABLE = " SystemConfig ";
         private const string INSERT_PARAMS = " (CfgKey, CfgValue, DisplayName) VALUES(@cfgkey,@cfgvalue,@displayname) ";
         private const string UPDATE_WHERES = " WHERE CfgKey = @keyname ";
@@ -104,8 +112,123 @@ namespace InteliPhoneBookService
             return bFound;
         }
 
+        void LoadLicFile(out string p_lictypedesc)
+        {
+            p_lictypedesc = "3";
+            string versiontypestr = "";
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load("C:\\src\\Inteli-Phone-Book\\InteliPhoneBook\\InteliPhoneBookService\\bin\\Debug\\lic.xml");
+
+            string endpoint = xdoc.DocumentElement["endpoint"].InnerText;
+            AddKey = DecryptDES_ProjectInsideKey(endpoint, "35405717");
+            /*xdoc.DocumentElement["type"].InnerText;
+            xdoc.DocumentElement["createtime"].InnerText;
+            xdoc.DocumentElement["endtime"].InnerText;
+            xdoc.DocumentElement["guid"].InnerText;*/
+            XmlElement elem = (XmlElement)xdoc.DocumentElement["features"].FirstChild;
+            int nFeatures = xdoc.DocumentElement["features"].GetElementsByTagName("feature").Count;
+            for (int i = 0; i < nFeatures; i++)
+            {
+                if (elem.Attributes["name"].Value == "versiontype")
+                    versiontypestr = DecryptDES(elem.Attributes["value"].Value, "35405717");
+                elem = (XmlElement)elem.NextSibling;
+            }
+            //xdoc.DocumentElement["signature"].InnerText;
+
+            if (versiontypestr == "basic")//SJ966WcqFE8=
+            {
+                p_lictypedesc = "4";
+                log.Info("basic lic mode");
+            }
+            else if (versiontypestr == "enforce")//RH2ah1pD+oo=
+            {
+                p_lictypedesc = "5";
+                log.Info("enforce lic mode");
+            }
+            else if (versiontypestr == "custom")//EpeEkWdlzb4=
+            {
+                p_lictypedesc = "6";
+                log.Info("custom lic mode");
+            }
+            else
+            {
+                p_lictypedesc = "0";
+                log.Info("evaluation lic mode." + versiontypestr);
+            }
+        }
+
+        string DecryptDES_ProjectInsideKey(string decryptString, string decryptKey)
+        {
+
+            try
+            {
+                if (decryptKey.Length < 8)
+                {
+                    decryptKey += ProjectInsideAddKey;
+                }
+                byte[] rgbKey = Encoding.UTF8.GetBytes(decryptKey.Substring(0, 8));
+
+                byte[] rgbIV = ProjectInsideKeys;
+
+                byte[] inputByteArray = Convert.FromBase64String(decryptString);
+
+                DESCryptoServiceProvider DCSP = new DESCryptoServiceProvider();
+
+                MemoryStream mStream = new MemoryStream();
+
+                CryptoStream cStream = new CryptoStream(mStream, DCSP.CreateDecryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+
+                cStream.FlushFinalBlock();
+
+                return Encoding.UTF8.GetString(mStream.ToArray());
+
+            }
+            catch
+            {
+                return decryptString;
+            }
+        }
+
+
+        string DecryptDES(string decryptString, string decryptKey)
+        {
+
+            try
+            {
+                if (decryptKey.Length < 8)
+                {
+                    decryptKey += AddKey;
+                }
+                byte[] rgbKey = Encoding.UTF8.GetBytes(decryptKey.Substring(0, 8));
+
+                byte[] rgbIV = Keys;
+
+                byte[] inputByteArray = Convert.FromBase64String(decryptString);
+
+                DESCryptoServiceProvider DCSP = new DESCryptoServiceProvider();
+
+                MemoryStream mStream = new MemoryStream();
+
+                CryptoStream cStream = new CryptoStream(mStream, DCSP.CreateDecryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+
+                cStream.FlushFinalBlock();
+
+                return Encoding.UTF8.GetString(mStream.ToArray());
+
+            }
+            catch
+            {
+                return decryptString;
+            }
+        }
+
         static public void DoWork(Object stateInfo)
         {
+            string lictypedesc;
             LicProcessor licProcessor = (LicProcessor)stateInfo;
             LicProcessor.LicProcessorObj = licProcessor;
             
@@ -113,8 +236,12 @@ namespace InteliPhoneBookService
             {
                 if (InteliPhoneBookService.ServiceIsTerminating == 1)
                 { Interlocked.Increment(ref InteliPhoneBookService.LicThreadTerminated); break; }
-                LicProcessorObj.UpdateLicInfo("4", "5");
-                Thread.Sleep(1000);
+                LicProcessorObj.LoadLicFile(out lictypedesc);
+                if ( lictypedesc == "0" )
+                    LicProcessorObj.UpdateLicInfo("3", lictypedesc);
+                else
+                    LicProcessorObj.UpdateLicInfo("4", lictypedesc);
+                Thread.Sleep(60000);
             }
             log.Info("exited\r\n");
         }
