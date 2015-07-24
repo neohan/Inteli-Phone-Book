93fffbabd96e4a1f360a822b86986588bff18bda
neohan <neo_han@sohu.com>
2015-07-24 16:38:12 +0800

    增加一个安装许可文件的程序。

---
+++ InteliPhoneBook/InstallLicenseFile/FormInstallLicenseFile.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace InstallLicenseFile
+{
+    partial class FormInstallLicenseFile
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonInstallLicFile = new System.Windows.Forms.Button();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonInstallLicFile
+            // 
+            this.buttonInstallLicFile.Location = new System.Drawing.Point(204, 7);
+            this.buttonInstallLicFile.Name = "buttonInstallLicFile";
+            this.buttonInstallLicFile.Size = new System.Drawing.Size(145, 20);
+            this.buttonInstallLicFile.TabIndex = 0;
+            this.buttonInstallLicFile.Text = "安装";
+            this.buttonInstallLicFile.UseVisualStyleBackColor = true;
+            this.buttonInstallLicFile.Click += new System.EventHandler(this.buttonInstallLicFile_Click);
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.ItemHeight = 12;
+            this.listBoxLog.Location = new System.Drawing.Point(12, 34);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(530, 220);
+            this.listBoxLog.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "lic";
+            this.openFileDialog1.Filter = "许可文件|*.xml";
+            // 
+            // FormInstallLicenseFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 273);
+            this.Controls.Add(this.listBoxLog);
+            this.Controls.Add(this.buttonInstallLicFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormInstallLicenseFile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "安装许可文件";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonInstallLicFile;
+        private System.Windows.Forms.ListBox listBoxLog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ InteliPhoneBook/InstallLicenseFile/FormInstallLicenseFile.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Security.Cryptography;
+
+/*
+ * 不只是将许可文件安装到指定目录，在拷贝前还必须检查许可是否合法，等于将核心程序使用检查许可的方法再拷贝一份到这个许可安装程序内。
+ * 如果目标目录内已存在一个合法的许可文件，还需提示是否需要替换。
+ */
+
+namespace InstallLicenseFile
+{
+    public partial class FormInstallLicenseFile : Form
+    {
+        private static byte[] ProjectInsideKeys = { 0xAA, 0xDF, 0x29, 0x5C, 0x36, 0x90, 0x17, 0x68 };
+        private static string ProjectInsideAddKey = "projectINSIDEkey";
+        private static byte[] Keys = { 0xA5, 0x95, 0x6E, 0x44, 0x80, 0xCB, 0x8F, 0x77 };
+        private static string AddKey = "";
+
+        public FormInstallLicenseFile()
+        {
+            InitializeComponent();
+        }
+
+        string DecryptDES_ProjectInsideKey(string decryptString, string decryptKey)
+        {
+
+            try
+            {
+                if (decryptKey.Length < 8)
+                {
+                    decryptKey += ProjectInsideAddKey;
+                }
+                byte[] rgbKey = Encoding.UTF8.GetBytes(decryptKey.Substring(0, 8));
+
+                byte[] rgbIV = ProjectInsideKeys;
+
+                byte[] inputByteArray = Convert.FromBase64String(decryptString);
+
+                DESCryptoServiceProvider DCSP = new DESCryptoServiceProvider();
+
+                MemoryStream mStream = new MemoryStream();
+
+                CryptoStream cStream = new CryptoStream(mStream, DCSP.CreateDecryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+
+                cStream.FlushFinalBlock();
+
+                return Encoding.UTF8.GetString(mStream.ToArray());
+
+            }
+            catch
+            {
+                return decryptString;
+            }
+        }
+
+        string DecryptDES(string decryptString, string decryptKey)
+        {
+
+            try
+            {
+                if (decryptKey.Length < 8)
+                {
+                    decryptKey += AddKey;
+                }
+                byte[] rgbKey = Encoding.UTF8.GetBytes(decryptKey.Substring(0, 8));
+
+                byte[] rgbIV = Keys;
+
+                byte[] inputByteArray = Convert.FromBase64String(decryptString);
+
+                DESCryptoServiceProvider DCSP = new DESCryptoServiceProvider();
+
+                MemoryStream mStream = new MemoryStream();
+
+                CryptoStream cStream = new CryptoStream(mStream, DCSP.CreateDecryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+
+                cStream.FlushFinalBlock();
+
+                return Encoding.UTF8.GetString(mStream.ToArray());
+
+            }
+            catch
+            {
+                return decryptString;
+            }
+        }
+
+        private void buttonInstallLicFile_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = openFileDialog1.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                XmlDocument xdoc = new XmlDocument();
+                try
+                {
+                    xdoc.Load(openFileDialog1.FileName);
+                }
+                catch (Exception ex)
+                {
+                    listBoxLog.Items.Add(String.Format("无效的许可文件。\r\n{0}", ex.Message));
+                    return;
+                }
+
+                //verification
+                try
+                {
+                    string endpoint = xdoc.DocumentElement["endpoint"].InnerText;
+                    AddKey = DecryptDES_ProjectInsideKey(endpoint, "35405717");
+
+                    SHA384Managed shaM = new SHA384Managed();
+                    byte[] data;
+
+                    MemoryStream ms = new MemoryStream();
+                    BinaryWriter bw = new BinaryWriter(ms);
+                    bw.Write(12);
+                    bw.Write(xdoc.DocumentElement["level"].InnerText);
+                    bw.Write(xdoc.DocumentElement["type"].InnerText);
+                    bw.Write(xdoc.DocumentElement["endpoint"].InnerText);
+                    bw.Write(xdoc.DocumentElement["createtime"].InnerText);
+                    bw.Write(xdoc.DocumentElement["endtime"].InnerText);
+                    bw.Write(xdoc.DocumentElement["guid"].InnerText);
+                    XmlElement elem = (XmlElement)xdoc.DocumentElement["features"].FirstChild;
+                    int nFeatures = xdoc.DocumentElement["features"].GetElementsByTagName("feature").Count;
+                    for (int i = 0; i < nFeatures; i++)
+                    {
+                        bw.Write(elem.Attributes["name"].Value); bw.Write(elem.Attributes["value"].Value);
+                        if (elem.Attributes["name"].Value == "versiontype")
+                            DecryptDES(elem.Attributes["value"].Value, "35405717");
+                        if (elem.Attributes["name"].Value == "siptrunk")
+                            DecryptDES(elem.Attributes["value"].Value, "35405717");
+                        elem = (XmlElement)elem.NextSibling;
+                    }
+                    int nLen = (int)ms.Position + 1;
+                    bw.Close();
+                    ms.Close();
+                    data = ms.GetBuffer();
+
+                    data = shaM.ComputeHash(data, 0, nLen);
+
+                    string result = "";
+                    foreach (byte dbyte in data)
+                    {
+                        result += dbyte.ToString("X2");
+                    }
+                    string signature = xdoc.DocumentElement["signature"].InnerText;
+                    if (signature != result)
+                    {
+                        listBoxLog.Items.Add("无效的许可文件。签名错。");
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    listBoxLog.Items.Add(String.Format("无效的许可文件。\r\n{0}", ex.Message));
+                    return;
+                }
+            }
+        }
+    }
+}
